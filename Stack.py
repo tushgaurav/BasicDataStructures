0abb8ecf45d22d5d85bd0bc759af11c7a49f6169
tushgaurav <tushar.gaurav416@gmail.com>
2022-01-06 18:57:57 +0530

    added stack

---
+++ Stack.py
@@ -0,0 +1,16 @@
+stack = []
+
+# using append() to push
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+print("The stack is", stack)
+
+# using pop() to remove the last inserted item, LIFO order
+stack.pop()
+print("The stack is", stack)
+
+stack.pop()
+print("The stack is", stack)
